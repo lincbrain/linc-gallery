09748dbef09e4339b70d787ac329030375dffcbc
Kabilar Gunalan <kabi@mit.edu>
2026-02-04 11:32:52 -0600

    Add `dmri-xray` page to top navigation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,8 @@ const config = {
         },
         items: [
           {to: '/pathways', label: 'Pathway Atlas', position: 'right'},
-          {to: '/dmri', label: 'High-resolution dMRI', position: 'right'},
+          {to: '/dmri', label: 'Diffusion MRI', position: 'right'},
+          {to: '/dmri-xray', label: 'Diffusion MRI + X-ray', position: 'right'},
           {to: '/team', label: 'Team', position: 'right'},
           {to: 'https://connects.mgh.harvard.edu/', label: 'Project Homepage', position: 'right'},
         ],
